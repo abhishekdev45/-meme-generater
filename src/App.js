c78e65b8e40dbbd469d4732d27d364f5241678de
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2023-05-01 18:07:22 +0530

    Added files for meme generator mini-project.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header'
+import Meme from './components/meme'
+import './app.css'
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Header() {
+    return (
+      <header className="header">
+        <img src="https://icon-library.com/images/2018/143591_meme-fsjal-meme-hd-png-download.png" alt="logo" className='header--img'/>
+        <h2 className='header--title'>Meme Generater</h2>
+        <h3 className='header--text'>Abhishek Rajbhar</h3>
+      </header>
+    );
+  }
+  
+  export default Header;
